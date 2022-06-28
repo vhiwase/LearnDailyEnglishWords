fbb9caf22bfd5ef834340b4394559fc8a646ba3b
Vaibhav Hiwase <vaibhav.hiwase@celebaltech.com>
2022-06-28 08:26:19 +0530

    adding docker for creating database

--- README.md
+++ README.md
@@ -16,7 +16,15 @@ Hosted on Azure Container App with CI/CD and OAuth security
 Link of Application: https://learn-daily-english-words.delightfulforest-b9dc5d98.eastasia.azurecontainerapps.io
 
 ## Generate database for quick loading
-Run the following command to quickly load vocabulary words:
+Make sure you first intall depedencies from [blog post](https://testdriven.io/blog/building-a-concurrent-web-scraper-with-python-and-selenium/).
+
+Then Run the following command to quickly load vocabulary words:
 ```sh
 python3 server/database.py
+```
+
+Alternatively you can use docker compose commands:
+```sh
+docker-compose -f docker-compose.database.yml up --build -d
+docker-compose -f docker-compose.database.yml down -v
 ```
--- server/database.py
+++ server/database.py
@@ -27,21 +27,6 @@ from webdriver_manager.chrome import (
 JSON_PATH = (BASE_PATH / "important_words.json").absolute().as_posix()
 DATABASE_PATH = (BASE_PATH / "database.json").absolute().as_posix()
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
-try:
-    driver = webdriver.Chrome(options=chrome_options)
-except Exception as e:
-    print(e)
-    driver = webdriver.Chrome(
-        ChromeDriverManager().install(), options=chrome_options
-    )  # chrome_options is deprecated
-    driver.maximize_window()
-
 ROOT_PATH = pathlib.Path(__file__)
 BASE_PATH = ROOT_PATH.parent
 
@@ -321,4 +306,19 @@ def run():
 
 
 if __name__ == "__main__":
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    try:
+        driver = webdriver.Chrome(options=chrome_options)
+    except Exception as e:
+        print(e)
+        driver = webdriver.Chrome(
+            ChromeDriverManager().install(), options=chrome_options
+        )  # chrome_options is deprecated
+        driver.maximize_window()
     run()
+    driver.close()
